815d868fb9f0e6d45e488cdb9a598e3e1368dc0a
François MATHIEU <francois.mathieu@livexp.fr>
2018-09-24 16:12:14 +0200

    Add Deck and Profile

--- core/admin.py
+++ core/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Card, CardSet, CardType
+from .models import Card, CardSet, CardType, Deck
 
 admin.site.register(Card)
 admin.site.register(CardSet)
 admin.site.register(CardType)
+admin.site.register(Deck)
--- core/models.py
+++ core/models.py
@@ -1,7 +1,12 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-
 # Create your models here.
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
+
+from accounts.models import Profile
+
 
 class CardSet(models.Model):
     name = models.CharField(max_length=256)
@@ -30,3 +35,18 @@ class Card(models.Model):
 
     def __str__(self):
         return "[%s] - %s" % (self.card_id, self.name)
+
+
+class Deck(models.Model):
+    name = models.CharField(max_length=256, default=None, blank=True, null=True)
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    cards = models.ManyToManyField(Card)
+
+    def __str__(self):
+        return "%s (%s)" % (self.name, self.cards.count())
+
+
+@receiver(pre_save, sender=Deck)
+def pre_save_deck(sender, instance, **kwargs):
+    if 8 > instance.cards.count():
+        raise AttributeError("A deck cannot have more than 8 cards !")
